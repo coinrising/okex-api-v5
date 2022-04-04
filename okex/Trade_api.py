c2a60ec66f3f737f45f9a1ac0665ece4c74b7bdb
marz <marzwu@gmail.com>
2022-04-04 12:26:25 +0800

    + tgtCcy

--- okex/Trade_api.py
+++ okex/Trade_api.py
@@ -9,9 +9,9 @@ class TradeAPI(Client):
 
     # Place Order
     def place_order(self, instId, tdMode, side, ordType, sz, ccy=None, clOrdId=None, tag=None, posSide=None, px=None,
-                    reduceOnly=None):
+                    reduceOnly=None, tgtCcy=None):
         params = {'instId': instId, 'tdMode': tdMode, 'side': side, 'ordType': ordType, 'sz': sz, 'ccy': ccy,
-                  'clOrdId': clOrdId, 'tag': tag, 'posSide': posSide, 'px': px, 'reduceOnly': reduceOnly}
+                  'clOrdId': clOrdId, 'tag': tag, 'posSide': posSide, 'px': px, 'reduceOnly': reduceOnly, 'tgtCcy': tgtCcy}
         return self._request_with_params(POST, PLACR_ORDER, params)
 
     # Place Multiple Orders
